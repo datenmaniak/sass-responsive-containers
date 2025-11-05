7b713481181f401b6e5ef70f32081ed48546e7d3
datenmaniak <ppwj@yahoo.com>
2025-11-05 17:19:18 -0400

    Added: menuToggle

--- js/hide-menu.js
+++ js/hide-menu.js
@@ -1,15 +1,16 @@
-document.querySelector('.menuToggle')?.addEventListener('click', () => {
-  const nav = document.querySelector('.main-header__nav');
-  const isVisible = !nav.hasAttribute('hidden');
+document.querySelector(".menuToggle")?.addEventListener("click", () => {
+  const nav = document.querySelector(".nav__options");
+  const isVisible = !nav.hasAttribute("hidden");
 
-  nav.toggleAttribute('hidden');
-  console.log(`[Violet Pulse] Menu ${isVisible ? 'cerrado' : 'abierto'} correctamente.`);
+  nav.toggleAttribute("hidden");
+  console.log(
+    `[Violet Pulse] Menu ${isVisible ? "cerrado" : "abierto"} correctamente.`
+  );
 });
 
 function toggleMenu() {
-  const menuToggle = document.querySelector('.menuToggle');
-  const navbar = document.querySelector('.main-header__nav');
-  menuToggle.classList.toggle('active');
-  navbar.classList.toggle('active');
-
+  const menuToggle = document.querySelector(".menuToggle");
+  const navbar = document.querySelector(".nav__options");
+  menuToggle.classList.toggle("active");
+  navbar.classList.toggle("active");
 }
